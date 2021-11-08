3b61750a8d6f9e5b353629fe909c6da2e2f8dc36
zephyr <zzzzzzzephyr@gmail.com>
2021-11-08 13:35:10 +0800

    adaptor

---
+++ src/main/java/com/zephyraft/dp/_2struct/_4adapter/_help/Adaptee.java
@@ -0,0 +1,12 @@
+package com.zephyraft.dp._2struct._4adapter._help;
+
+public class Adaptee {
+    public void fa() {
+    }
+
+    public void fb() {
+    }
+
+    public void fc() {
+    }
+}
---
+++ src/main/java/com/zephyraft/dp/_2struct/_4adapter/_help/ITarget.java
@@ -0,0 +1,7 @@
+package com.zephyraft.dp._2struct._4adapter._help;
+
+public interface ITarget {
+    void f1();
+    void f2();
+    void f3();
+}
